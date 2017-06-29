539e331e68c08e0428a8d5ebfa48620a567484fc
Kevin Wong <kevinwong6175@gmail.com>
2017-06-29 18:10:53 +0000

    CRUD Spam

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,12 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @posts[0].title = "SPAM"
+    @posts.each do |post|
+      if post.id % 5 == 0
+        post.title = "SPAM"
+      end
+    end
   end
 
   def show
