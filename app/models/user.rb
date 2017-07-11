11ab56bc5efcf9f55878989f23cf95f39c05cb79
Kevin Wong <kevinwong6175@gmail.com>
2017-07-11 02:15:50 +0000

    Validate Name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
     before_save { self.email = email.downcase if email.present? }
+    before_save { self.name = name.split.map{|word| word.capitalize}.join(' ') if name}
     validates :name, length: { minimum: 1, maximum: 100 }, presence: true
     validates :password, presence: true, length: { minimum: 6}, if: "password_digest.nil?"
     validates :password, length: { minimum: 6 }, allow_blank: true
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  let(:user) { User.create!(name: "Bloccit User", email: "user@bloccit.com", password: "password") }
+  let(:user) { User.create!(name: "bloccit user", email: "user@bloccit.com", password: "password") }
   
   it { is_expected.to validate_presence_of(:name) }
   it { is_expected.to validate_length_of(:name).is_at_least(1) }
