717365ebfd5cbaaf027e3eb6b14c65d1125e1794
Kevin Wong <kevinwong6175@gmail.com>
2017-07-12 18:41:35 +0000

    Sign In

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -48,5 +48,10 @@ RSpec.describe UsersController, type: :controller do
             post :create, user: new_user_attributes
             expect(assigns(:user).password).to eq new_user_attributes[:password]
         end
+        
+        it "logs the user in after sign up" do
+            post :create, user: new_user_attributes
+            expect(session[:user_id]).to eq assigns(:user).id
+        end
     end
 end
